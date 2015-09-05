5b23e8c74ee7f6f58c5c7416143e77d011b715f5
khaliqqq <akhaliq3@live.utm.my>
2015-09-05 18:43:22 +0800

    done step 4

--- Matching-Game/Matching-Game/Form1.cs
+++ Matching-Game/Matching-Game/Form1.cs
@@ -12,9 +12,46 @@ namespace Matching_Game
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
+        List<string> icons = new List<string>
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquare();
+        }
+
+        public void AssignIconsToSquare()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if(iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+
+                }
+            }
+        }
+
+        private void label_click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+            if(clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                clickedLabel.ForeColor = Color.Black;
+            }
         }
     }
 }
