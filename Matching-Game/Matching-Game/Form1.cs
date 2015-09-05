add365adac5e8236f7ff3a3c458926bb557e51c0
khaliqqq <akhaliq3@live.utm.my>
2015-09-05 18:50:09 +0800

    step 5 done

--- Matching-Game/Matching-Game/Form1.cs
+++ Matching-Game/Matching-Game/Form1.cs
@@ -20,6 +20,9 @@ namespace Matching_Game
             "b", "b", "v", "v", "w", "w", "z", "z"
         };
 
+        Label firstClicked = null;
+        Label secondClicked = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -42,15 +45,33 @@ namespace Matching_Game
             }
         }
 
+        /// <summary> 
+        /// Every label's Click event is handled by this event handler 
+        /// </summary> 
+        /// <param name="sender">The label that was clicked</param>
+        /// <param name="e"></param>
         private void label_click(object sender, EventArgs e)
         {
             Label clickedLabel = sender as Label;
+
             if(clickedLabel != null)
             {
+                // If the clicked label is black, the player clicked 
+                // an icon that's already been revealed -- 
+                // ignore the click 
                 if (clickedLabel.ForeColor == Color.Black)
                     return;
 
-                clickedLabel.ForeColor = Color.Black;
+                // If firstClicked is null, this is the first icon  
+                // in the pair that the player clicked, 
+                // so set firstClicked to the label that the player  
+                // clicked, change its color to black, and return
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
             }
         }
     }
